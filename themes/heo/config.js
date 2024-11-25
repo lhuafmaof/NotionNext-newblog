9626be2ccb30b012ce726af2940e06b3472295fe
易亚洲 <609895092@qq.com>
2024-11-25 13:59:03 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享思考',
+  HEO_HERO_TITLE_2: '与俯身实践',
+  HEO_HERO_TITLE_3: 'LHUAFMAOF.COM',
+  HEO_HERO_TITLE_4: '',
+  HEO_HERO_TITLE_5: '西子湖畔 养意蓄力',
+  HEO_HERO_TITLE_LINK: 'https://www.lhuafmaof.cn/',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
