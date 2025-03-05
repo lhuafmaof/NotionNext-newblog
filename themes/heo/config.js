f98180228c2fe5e46e967e9e7f48341d3daa6a4d
易亚洲 <609895092@qq.com>
2025-03-05 23:22:12 +0800

    Update config.js
    3.5日 1.0更新

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.lhuafmaof.cn/' },
+    { title: '访问文档中心获取更多帮助', url: 'https://www.lhuafmaof.cn/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -17,29 +17,33 @@ const CONFIG = {
   // 博客主体区左右侧组件颠倒位置
   HEO_HERO_BODY_REVERSE: false,
 
+
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享思考',
+  HEO_HERO_TITLE_2: '与俯身实践',
+  HEO_HERO_TITLE_3: 'LHUAFMAOF.COM',
+  HEO_HERO_TITLE_4: '',
+  HEO_HERO_TITLE_5: '西子湖畔 养意蓄力',
+  HEO_HERO_TITLE_LINK: 'https://www.lhuafmaof.cn/',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
+  
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '互联网观察', url: '/tag/互联网观察' },
+  HEO_HERO_CATEGORY_2: { title: 'GPT+AI+Notion', url: '/tag/GPT+AI+Notion' },
+  HEO_HERO_CATEGORY_3: { title: '低代码+自动化+B端', url: '/tag/低代码+自动化+B端' },
+  
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
-  // 右侧个人资料卡牌欢迎语，点击可自动切换
+
+ // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！我是令狐',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -47,7 +51,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-
+  
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
